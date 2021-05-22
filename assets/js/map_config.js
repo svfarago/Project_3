c5fe466ba146363ade1473219b9f24f5201e75d6
aolea13 <austinolea@gmail.com>
2021-05-22 14:12:08 -0600

    :sparkles: Razzle Dazzle

--- assets/js/map_config.js
+++
@@ -1,2 +0,0 @@
-// API key
-const API_KEY = "pk.eyJ1IjoianJvc2VkYXZpcyIsImEiOiJja242dXlvb28waTJjMm9tMXM3a3h1NnZhIn0.KFACWynEe0Oq4-o1xCtLnA";
--- assets/js/weather_config.js
+++
@@ -1,2 +0,0 @@
-//API Key
-const API_KEY = "f3ad52969d96a88aec01f1b67be1643a"
